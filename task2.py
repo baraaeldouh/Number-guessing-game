c085e661d5c676f199ce35a923d091f4336f4130
baraaeldouh <baraaeldouh@gmail.com>
2026-06-15 15:03:55 +0300

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,18 @@
+num1=int(input("enter first number"))
+num2=int(input("enter second number"))
+oper=input("choose an operation (+,-,/,*)")
+if oper == "+":
+    result=num1+num2
+    print(num1,"+" ,num2,"=",result)
+elif oper=="-":
+    result=num1-num2
+    print(num1,"-" ,num2,"=",result)
+elif oper=="*":
+    result=num1*num2
+    print(num1,"*" ,num2,"=",result)
+
+
+
+
+
+    
